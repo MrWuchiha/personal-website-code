c2ba1ae6d72fbc2dd88a6fc989e6d3ff76f43a05
edwu1 <edmundzjwu@gmail.com>
2022-09-03 12:05:42 -0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'personal-website',
+  base: '/personal-website/',
   build: {
     chunkSizeWarningLimit: 3000,
   }
